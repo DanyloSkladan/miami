ebc7321212bcab89110625ad07eac27b2c6eadc1
DanyloSkladan <dskladannnnn09082001@gmail.com>
2023-12-02 01:27:07 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+const sliderMain = new Swiper('.slider_main', {
+    freeMode: true,
+    centeredSlides: true,
+    breakpoints:{
+        0: {
+            slidesPerView: 2.5
+        }
+    }
+})
